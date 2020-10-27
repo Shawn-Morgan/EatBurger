4796371b8c0d26da7f801ff499d23ecd02c08dc6
shawnm7474 <shawnm74@gmail.com>
2020-10-27 15:29:17 -0500

    testing heroku deployment fix

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,26 @@ var mysql = require("mysql");
 
 var connection;
 
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+//   } else{
+//     connection = mysql.createConnection({
+//       host: "localhost",
+//       port: 3306,
+//       user: "root",
+//       password: "RemakeMyself2020&",
+//       database: "burger_db"
+//   })
+// }
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else{
     connection = mysql.createConnection({
-      host: "localhost",
+      host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       port: 3306,
-      user: "root",
-      password: "RemakeMyself2020&",
+      user: "y6wwtqv1177x5r9a",
+      password: "e4yuzp09ex3o7rjw",
       database: "burger_db"
   })
 }
