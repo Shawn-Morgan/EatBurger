77afbdc7bb6caecd448912e7e6038d5684b2649c
shawnm7474 <shawnm74@gmail.com>
2020-10-27 16:00:24 -0500

    heroku deploy issue again

--- config/connection.js
+++ config/connection.js
@@ -4,26 +4,14 @@ var mysql = require("mysql");
 
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-//   } else{
-//     connection = mysql.createConnection({
-//       host: "localhost",
-//       port: 3306,
-//       user: "root",
-//       password: "RemakeMyself2020&",
-//       database: "burger_db"
-//   })
-// }
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else{
     connection = mysql.createConnection({
-      host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      host: "localhost",
       port: 3306,
-      user: "y6wwtqv1177x5r9a",
-      password: "e4yuzp09ex3o7rjw",
+      user: "root",
+      password: "RemakeMyself2020&",
       database: "burger_db"
   })
 }
